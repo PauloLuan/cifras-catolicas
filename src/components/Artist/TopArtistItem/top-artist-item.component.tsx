00f72012ea0f8c608e1952f1ce026a3193291dd9
Paulo Luan <pauloluan.inova@gmail.com>
2021-07-08 05:12:39 -0300

    ✨ feat: added initial version of top music item

--- src/components/Artist/TopArtistItem/top-artist-item.component.tsx
+++ src/components/Artist/TopArtistItem/top-artist-item.component.tsx
@@ -1,4 +1,11 @@
-import { Flex } from '@chakra-ui/react'
+import {
+  Center,
+  Box,
+  chakra,
+  Flex,
+  useColorModeValue,
+  VStack
+} from '@chakra-ui/react'
 
 export interface TopArtistItemProps {
   name?: string
@@ -6,8 +13,51 @@ export interface TopArtistItemProps {
 
 export const TopArtistItem = ({ name }: TopArtistItemProps) => {
   return (
-    <Flex>
-      <h1 data-testid='top-artist-item'>top-artist-item</h1>
+    <Flex
+      w="full"
+      alignItems="center"
+      justifyContent="center"
+      shadow="lg"
+      rounded="lg"
+      _hover={{ bg: 'orange.50' }}
+      role="group"
+      colorScheme="orange"
+      cursor="pointer"
+    >
+      <Box w={1 / 3}>
+        <Center>
+          <chakra.h1
+            data-testid="top-artist-item"
+            fontSize="5xl"
+            fontWeight="bold"
+            color={useColorModeValue('gray.400', 'white')}
+            _groupHover={{ color: 'orange.500' }}
+          >
+            1
+          </chakra.h1>
+        </Center>
+      </Box>
+      <Box w={2 / 3}>
+        <VStack p={4}>
+          <Center>
+            <chakra.strong
+              fontSize="md"
+              color={useColorModeValue('gray.600', 'gray.400')}
+              _groupHover={{ color: 'orange.500' }}
+            >
+              Porque ele vive
+            </chakra.strong>
+          </Center>
+          <Center>
+            <chakra.span
+              fontSize="sm"
+              color={useColorModeValue('gray.600', 'gray.400')}
+            >
+              Harpa Cristã
+            </chakra.span>
+          </Center>
+        </VStack>
+      </Box>
     </Flex>
   )
 }
